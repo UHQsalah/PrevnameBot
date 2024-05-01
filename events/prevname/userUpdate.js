8b6e63d55512df26b79c328f641a1b18ae0ce734
salahW3b <113988161+salahW3b@users.noreply.github.com>
2024-05-01 18:37:21 +0200

    AJOUT GlobalName

--- events/prevname/userUpdate.js
+++ events/prevname/userUpdate.js
@@ -3,4 +3,11 @@ module.exports = async (client, db, config, oldUser, newUser) => {
         db.set(`prevname_${oldUser.id}_${parseInt(new Date() / 1000)}_${newUser.username}`, true);
         console.log(`${oldUser.username} => ${newUser.username}`);
     }
+
+    if (oldUser.globalName !== newUser.globalName) {
+        const prevname = oldUser.globalName;
+        const userId = oldUser.id;
+        console.log(`Prevname Detection: ${oldUser.globalName} --> ${newUser.globalName}`);
+        db.set(`prevname_${userId}_${parseInt(new Date() / 1000)}_${prevname}`, true);
+    }
 };
